ad6c0acc5947df223eb1ffff621321569e01809d
windy <kugoo11v12@126.com>
2020-11-23 19:21:16 +0800

    [feature] cocostouchpad 支持反注册

--- src/uit/CocosTouchPad.ts
+++ src/uit/CocosTouchPad.ts
@@ -21,10 +21,73 @@ namespace gcc.uit {
 		guesture: kitten.guesture.GuestureAnalyzer
 		scrollData: ScrollData
 
+		protected listenEvents: { [key: string]: Function }
 		init() {
 			this.guesture = new kitten.guesture.GuestureAnalyzer().init()
 			this.scrollData = new ScrollData()
+			this.listenEvents = fsync.EmptyTable()
+
+			this.initEvents()
+
 			return this
+		}
+
+		protected initEvents() {
+			const touchedPointMap: { [key: string]: boolean } = {}
+
+			this.listenEvents[cc.Node.EventType.TOUCH_START] = (event: cc.Event.EventTouch) => {
+				// console.log("touchstart", event)
+				let touches = event.getTouches()
+				let vecs: fsync.Vector3[] = []
+				touches.forEach((touch) => {
+					let vec = this.getTouchEventPos(touch)
+					vecs[touch.getID()] = vec
+					touchedPointMap[touch.getID()] = true
+				})
+				this.testGuesture2(vecs)
+				this.guesture.inputTouchPoints(true, vecs)
+			}
+
+			const onTouchOver = (event: cc.Event.EventTouch, key: string) => {
+				// console.log("touchend", event)
+				let touches = event.getTouches()
+				let vecs: fsync.Vector3[] = []
+				touches.forEach((touch) => {
+					let vec = this.getTouchEventPos(touch)
+					vecs[touch.getID()] = vec
+					touchedPointMap[touch.getID()] = false
+				})
+				this.testGuesture2(vecs)
+				this.guesture.inputTouchPoints(false, vecs)
+			}
+			const onTouchEnd = (event: cc.Event.EventTouch) => {
+				onTouchOver(event, "TOUCH_END")
+			}
+			const onTouchCancel = (event: cc.Event.EventTouch) => {
+				onTouchOver(event, "TOUCH_CANCEL")
+			}
+			this.listenEvents[cc.Node.EventType.TOUCH_CANCEL] = onTouchCancel
+			this.listenEvents[cc.Node.EventType.TOUCH_END] = onTouchEnd
+			this.listenEvents[cc.Node.EventType.TOUCH_MOVE] = (event: cc.Event.EventTouch) => {
+				// console.log("touchmove", event)
+				let touches = event.getTouches()
+				let vecs: fsync.Vector3[] = []
+				touches.forEach((touch) => {
+					// 屏蔽掉没有经由touchstart发起的触摸点，避免缩放bug
+					if (touchedPointMap[touch.getID()]) {
+						let vec = this.getTouchEventPos(touch)
+						vecs[touch.getID()] = vec
+					}
+				})
+				this.testGuesture2(vecs)
+				this.guesture.inputTouchPoints(true, vecs)
+			}
+			this.listenEvents[cc.Node.EventType.MOUSE_WHEEL] = (event: cc.Event.EventMouse) => {
+				console.log("mouse wheel", event)
+				this.scrollData.deltaScroll.x += event.getScrollX()
+				this.scrollData.deltaScroll.y += event.getScrollY()
+			}
+
 		}
 
 		/**
@@ -46,73 +109,85 @@ namespace gcc.uit {
 		}
 
 		/**
+		 * 接触侦听触摸事件
+		 */
+		unregisterTouchPad() {
+			this.listenInfo.forEach(info => {
+				let { node, useCapture } = info
+				for (let key in this.listenEvents) {
+					node.off(key, this.listenEvents[key], useCapture)
+				}
+			})
+			this.listenInfo.length = 0
+		}
+
+		protected listenInfo: { node: cc.Node, useCapture: boolean }[] = []
+
+		/**
 		 * 注册触摸板事件
 		 * @param touchPad
 		 * @param useCapture 将触摸或鼠标事件注册在捕获阶段
 		 */
 		registerTouchPad(touchPad: cc.Node, useCapture: boolean = false) {
-			const touchedPointMap: { [key: string]: boolean } = {}
+			// const touchedPointMap: { [key: string]: boolean } = {}
 
-			const handleTouchEvent = (isTouching: boolean, event: cc.Event.EventTouch) => {
-				let touches = event.getTouches()
-				let vecs: fsync.Vector3[] = []
-				touches.forEach((touch) => {
-					let vec = this.getTouchEventPos(touch)
-					vecs[touch.getID()] = vec
-				})
-				this.guesture.inputTouchPoints(isTouching, vecs)
+			// touchPad.on(cc.Node.EventType.TOUCH_START, (event: cc.Event.EventTouch) => {
+			// 	// console.log("touchstart", event)
+			// 	let touches = event.getTouches()
+			// 	let vecs: fsync.Vector3[] = []
+			// 	touches.forEach((touch) => {
+			// 		let vec = this.getTouchEventPos(touch)
+			// 		vecs[touch.getID()] = vec
+			// 		touchedPointMap[touch.getID()] = true
+			// 	})
+			// 	this.testGuesture2(vecs)
+			// 	this.guesture.inputTouchPoints(true, vecs)
+			// }, this, useCapture)
+			// const onTouchOver = (event: cc.Event.EventTouch, key: string) => {
+			// 	// console.log("touchend", event)
+			// 	let touches = event.getTouches()
+			// 	let vecs: fsync.Vector3[] = []
+			// 	touches.forEach((touch) => {
+			// 		let vec = this.getTouchEventPos(touch)
+			// 		vecs[touch.getID()] = vec
+			// 		touchedPointMap[touch.getID()] = false
+			// 	})
+			// 	this.testGuesture2(vecs)
+			// 	this.guesture.inputTouchPoints(false, vecs)
+			// }
+			// const onTouchEnd = (event: cc.Event.EventTouch) => {
+			// 	onTouchOver(event, "TOUCH_END")
+			// }
+			// const onTouchCancel = (event: cc.Event.EventTouch) => {
+			// 	onTouchOver(event, "TOUCH_CANCEL")
+			// }
+			// touchPad.on(cc.Node.EventType.TOUCH_CANCEL, onTouchCancel, this, useCapture)
+			// touchPad.on(cc.Node.EventType.TOUCH_END, onTouchEnd, this, useCapture)
+			// touchPad.on(cc.Node.EventType.TOUCH_MOVE, (event: cc.Event.EventTouch) => {
+			// 	// console.log("touchmove", event)
+			// 	let touches = event.getTouches()
+			// 	let vecs: fsync.Vector3[] = []
+			// 	touches.forEach((touch) => {
+			// 		// 屏蔽掉没有经由touchstart发起的触摸点，避免缩放bug
+			// 		if (touchedPointMap[touch.getID()]) {
+			// 			let vec = this.getTouchEventPos(touch)
+			// 			vecs[touch.getID()] = vec
+			// 		}
+			// 	})
+			// 	this.testGuesture2(vecs)
+			// 	this.guesture.inputTouchPoints(true, vecs)
+			// }, this, useCapture)
+			// touchPad.on(cc.Node.EventType.MOUSE_WHEEL, (event: cc.Event.EventMouse) => {
+			// 	console.log("mouse wheel", event)
+			// 	this.scrollData.deltaScroll.x += event.getScrollX()
+			// 	this.scrollData.deltaScroll.y += event.getScrollY()
+			// })
+
+			for (let key in this.listenEvents) {
+				touchPad.on(key, this.listenEvents[key], useCapture)
 			}
-			touchPad.on(cc.Node.EventType.TOUCH_START, (event: cc.Event.EventTouch) => {
-				// console.log("touchstart", event)
-				let touches = event.getTouches()
-				let vecs: fsync.Vector3[] = []
-				touches.forEach((touch) => {
-					let vec = this.getTouchEventPos(touch)
-					vecs[touch.getID()] = vec
-					touchedPointMap[touch.getID()] = true
-				})
-				this.testGuesture2(vecs)
-				this.guesture.inputTouchPoints(true, vecs)
-			}, this, useCapture)
-			const onTouchOver = (event: cc.Event.EventTouch, key: string) => {
-				// console.log("touchend", event)
-				let touches = event.getTouches()
-				let vecs: fsync.Vector3[] = []
-				touches.forEach((touch) => {
-					let vec = this.getTouchEventPos(touch)
-					vecs[touch.getID()] = vec
-					touchedPointMap[touch.getID()] = false
-				})
-				this.testGuesture2(vecs)
-				this.guesture.inputTouchPoints(false, vecs)
-			}
-			const onTouchEnd = (event: cc.Event.EventTouch) => {
-				onTouchOver(event, "TOUCH_END")
-			}
-			const onTouchCancel = (event: cc.Event.EventTouch) => {
-				onTouchOver(event, "TOUCH_CANCEL")
-			}
-			touchPad.on(cc.Node.EventType.TOUCH_CANCEL, onTouchCancel, this, useCapture)
-			touchPad.on(cc.Node.EventType.TOUCH_END, onTouchEnd, this, useCapture)
-			touchPad.on(cc.Node.EventType.TOUCH_MOVE, (event: cc.Event.EventTouch) => {
-				// console.log("touchmove", event)
-				let touches = event.getTouches()
-				let vecs: fsync.Vector3[] = []
-				touches.forEach((touch) => {
-					// 屏蔽掉没有经由touchstart发起的触摸点，避免缩放bug
-					if (touchedPointMap[touch.getID()]) {
-						let vec = this.getTouchEventPos(touch)
-						vecs[touch.getID()] = vec
-					}
-				})
-				this.testGuesture2(vecs)
-				this.guesture.inputTouchPoints(true, vecs)
-			}, this, useCapture)
-			touchPad.on(cc.Node.EventType.MOUSE_WHEEL, (event: cc.Event.EventMouse) => {
-				console.log("mouse wheel", event)
-				this.scrollData.deltaScroll.x += event.getScrollX()
-				this.scrollData.deltaScroll.y += event.getScrollY()
-			})
+
+			this.listenInfo.push({ node: touchPad, useCapture })
 		}
 
 	}
