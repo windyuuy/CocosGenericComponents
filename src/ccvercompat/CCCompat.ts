a15b889d8bd30b07353994fe6db9e2c09f1248ac
windy <kugoo11v12@126.com>
2021-06-04 14:37:13 +0800

    完善兼容性

---
+++ src/ccvercompat/CCCompat.ts
@@ -0,0 +1,161 @@
+
+// @ts-nocheck
+const {
+	ConeCollider, SimplexCollider,
+	AudioSource,
+	Camera,
+	Light,
+	MeshRenderer,
+	SkinnedMeshRenderer,
+	SkinnedMeshBatchRenderer,
+	SkinnedMeshUnit,
+	DirectionalLight,
+	SphereLight,
+	SpotLight,
+	Animation,
+	AnimationComponent,
+	SkeletalAnimation,
+	Billboard,
+	Line,
+	ParticleSystem,
+	Collider,
+	BoxCollider,
+	SphereCollider,
+	CapsuleCollider,
+	MeshCollider,
+	CylinderCollider,
+	RigidBody,
+	PhysicsMaterial,
+	Canvas,
+	UIRenderable,
+	UITransform,
+	Button,
+	EditBox,
+	Layout,
+	Mask,
+	ProgressBar,
+	RichText,
+	ScrollBar,
+	ScrollView,
+	Slider,
+	Sprite,
+	Toggle,
+	ToggleContainer,
+	UIMeshRenderer,
+	Widget,
+	LabelOutline,
+	Graphics,
+	PageView,
+	PageViewIndicator,
+	UIStaticBatch,
+	UIOpacity,
+	SafeArea,
+	UICoordinateTracker,
+	BlockInputEvents,
+	Label,
+} = cc
+
+//@ts-expect-error
+cc.Collider = Collider
+//@ts-expect-error
+cc.CapsuleCollider = CapsuleCollider
+//@ts-expect-error
+cc.SphereCollider = SphereCollider
+//@ts-expect-error
+cc.CylinderCollider = CylinderCollider
+//@ts-expect-error
+cc.ConeCollider = ConeCollider
+//@ts-expect-error
+cc.BoxCollider = BoxCollider
+//@ts-expect-error
+cc.SimplexCollider = SimplexCollider
+//@ts-expect-error
+cc.MeshCollider = MeshCollider
+//@ts-expect-error
+cc.RigidBody = RigidBody
+//@ts-expect-error
+cc.PhysicsMaterial = PhysicsMaterial
+//@ts-expect-error
+cc.AudioSource = AudioSource
+//@ts-expect-error
+cc.Camera = Camera
+//@ts-expect-error
+cc.Light = Light
+//@ts-expect-error
+cc.MeshRenderer = MeshRenderer
+//@ts-expect-error
+cc.SkinnedMeshRenderer = SkinnedMeshRenderer
+//@ts-expect-error
+cc.SkinnedMeshBatchRenderer = SkinnedMeshBatchRenderer
+//@ts-expect-error
+cc.SkinnedMeshUnit = SkinnedMeshUnit
+//@ts-expect-error
+cc.DirectionalLight = DirectionalLight
+//@ts-expect-error
+cc.SphereLight = SphereLight
+//@ts-expect-error
+cc.SpotLight = SpotLight
+//@ts-expect-error
+cc.Animation = Animation ?? AnimationComponent
+//@ts-expect-error
+cc.SkeletalAnimation = SkeletalAnimation
+//@ts-expect-error
+cc.Billboard = Billboard
+//@ts-expect-error
+cc.Line = Line
+//@ts-expect-error
+cc.ParticleSystem = ParticleSystem
+//@ts-expect-error
+cc.Canvas = Canvas
+//@ts-expect-error
+cc.UIRenderable = UIRenderable
+//@ts-expect-error
+cc.UITransform = UITransform
+//@ts-expect-error
+cc.Button = Button
+//@ts-expect-error
+cc.EditBox = EditBox
+//@ts-expect-error
+cc.Layout = Layout
+//@ts-expect-error
+cc.Mask = Mask
+//@ts-expect-error
+cc.ProgressBar = ProgressBar
+//@ts-expect-error
+cc.RichText = RichText
+//@ts-expect-error
+cc.ScrollBar = ScrollBar
+//@ts-expect-error
+cc.ScrollView = ScrollView
+//@ts-expect-error
+cc.Slider = Slider
+//@ts-expect-error
+cc.Sprite = Sprite
+//@ts-expect-error
+cc.Toggle = Toggle
+//@ts-expect-error
+cc.ToggleContainer = ToggleContainer
+//@ts-expect-error
+cc.UIMeshRenderer = UIMeshRenderer
+//@ts-expect-error
+cc.Widget = Widget
+//@ts-expect-error
+cc.LabelOutline = LabelOutline
+//@ts-expect-error
+cc.Graphics = Graphics
+//@ts-expect-error
+cc.PageView = PageView
+//@ts-expect-error
+cc.PageViewIndicator = PageViewIndicator
+//@ts-expect-error
+cc.UIStaticBatch = UIStaticBatch
+//@ts-expect-error
+cc.UIOpacity = UIOpacity
+//@ts-expect-error
+cc.SafeArea = SafeArea
+//@ts-expect-error
+cc.UICoordinateTracker = UICoordinateTracker
+//@ts-expect-error
+cc.BlockInputEvents = BlockInputEvents
+//@ts-expect-error
+cc.Label = Label
